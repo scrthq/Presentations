4ab8ed0652d5bbd950db5a808f52ea5d8284eae0
Nate Ferrell <nate@scrthq.com>
2018-09-17 21:21:39 -0500

    updating working code

--- StdResources/StdAutoScalingGroup.ps1
+++ StdResources/StdAutoScalingGroup.ps1
@@ -106,6 +106,31 @@ if ($PSBoundParameters.Keys -contains 'BucketName') {
     New-VSIAMPolicy @s3PolicyParams
 }
 
+if ($Global:VSConfig.Environment -eq 'prd') {
+    $topicParams = @{
+        LogicalId = 'StdPRDMonitoringTopic'
+        Subscription = @(
+            Add-VSSNSTopicSubscription -Endpoint (Add-FnJoin -Delimiter "" -ListOfValues "arn:aws:sqs:",(Add-FnRef $_AWSRegion),":",(Add-FnRef $_AWSAccountId),"MonitoringQueue") -Protocol 'sqs'
+        )
+        TopicName = (Add-FnJoin -Delimiter "" -ListOfValues 'Monitoring_Environment-',$global:VSConfig.Environment,'_Application-',$global:VSConfig.StackName)
+    }
+    New-VSSNSTopic @topicParams
+
+    $global:notificationConfig += Add-VSAutoScalingAutoScalingGroupNotificationConfiguration -NotificationTypes @(
+        'autoscaling:EC2_INSTANCE_LAUNCH'
+        'autoscaling:EC2_INSTANCE_LAUNCH_ERROR'
+        'autoscaling:EC2_INSTANCE_TERMINATE'
+        'autoscaling:EC2_INSTANCE_TERMINATE_ERROR'
+    ) -TopicARN (Add-FnRef 'StdPRDMonitoringTopic')
+
+    New-VaporResource -LogicalId "StdPRDMonitoringHostTemplate" -Type "Custom::MonitoringHostTemplate" -Properties @{
+        ServiceToken = (Add-FnJoin -Delimiter "" -ListOfValues "arn:aws:sns:",(Add-FnRef $_AWSRegion),":",(Add-FnRef $_AWSAccountId),"MonitoringHostTemplateTopic")
+        AccountId = (Add-FnRef $_AWSAccountId)
+        HostTemplateName = $global:VSConfig.StackName + "-" + $global:VSConfig.Environment
+        HostTemplateDescription = "Host template for " + $global:VSConfig.StackName + "-" + $global:VSConfig.Environment + " stack."
+    }
+}
+
 $elbParams = @{
     LogicalId = "StdElasticLoadBalancer"
     ConnectionDrainingPolicy = (Add-VSElasticLoadBalancingLoadBalancerConnectionDrainingPolicy -Enabled:$true -Timeout 300)
