b9d327157545023227130c850f8c5e32e6ef5ca3
Nate Ferrell <nate@scrthq.com>
2018-09-18 20:54:40 -0500

    updated code for readability

--- StdSqlExpressRDS.ps1
+++ StdSqlExpressRDS.ps1
@@ -1,21 +1,66 @@
 Import-Module VaporShell
 $template = Initialize-Vaporshell -Description "My SQL Server RDS stack"
 
-$customResource = New-VaporResource -LogicalId "SecretsManagerCustomResource" -Type "Custom::SecretsManager" -Properties @{
-    ServiceToken = (Add-FnJoin -Delimiter "" -ListOfValues 'arn:aws:lambda:',(Add-FnRef $_AWSRegion),':',(Add-FnRef $_AWSAccountId),':function:SecretsManagerCustomResource')
-    SecretId = 'development/RDS'
-    SecretKey = 'RDSMasterPassword'
+$newVaporResourceSplat = @{
+    Properties = @{
+        ServiceToken = (Add-FnJoin -Delimiter "" -ListOfValues @(
+            'arn:aws:lambda:'
+            (Add-FnRef $_AWSRegion)
+            ':'
+            (Add-FnRef $_AWSAccountId)
+            ':function:SecretsManagerCustomResource'))
+        SecretId = 'development/RDS'
+        SecretKey = 'RDSMasterPassword'
+    }
+    Type = "Custom::SecretsManager"
+    LogicalId = "SecretsManagerCustomResource"
 }
+$customResource = New-VaporResource @newVaporResourceSplat
 $secretValue = Add-FnGetAtt $customResource -AttributeName 'Secret'
 
-$securityGroupIngress = Add-VSEC2SecurityGroupIngress -CidrIp "$(Invoke-RestMethod http://ipinfo.io/json | Select-Object -ExpandProperty IP)/32" -FromPort '1433' -ToPort '1433' -IpProtocol 'tcp'
-$ec2SecurityGroup = New-VSEC2SecurityGroup -LogicalId 'RDSSecurityGroup' -GroupDescription 'Port 1433 access to RDS from local only' -SecurityGroupIngress $securityGroupIngress
+$addVSEC2SecurityGroupIngressSplat = @{
+    IpProtocol = 'tcp'
+    ToPort = '1433'
+    FromPort = '1433'
+    CidrIp = "$(Invoke-RestMethod http://ipinfo.io/json |
+                Select-Object -ExpandProperty IP)/32"
+}
+$securityGroupIngress = Add-VSEC2SecurityGroupIngress @addVSEC2SecurityGroupIngressSplat
 
-$rdsInstance = New-VSRDSDBInstance -LogicalId "SqlServerExpress" -MasterUsername 'rdsmaster' -MasterUserPassword $secretValue -DBInstanceClass 'db.t2.micro' -PubliclyAccessible $true -Engine 'sqlserver-ex' -MultiAZ $false -StorageType 'gp2' -EngineVersion "13.00.4451.0.v1" -DBInstanceIdentifier 'cf-sqlserver-ex-1' -AllocatedStorage '25' -AvailabilityZone 'us-west-2a' -VPCSecurityGroups (Add-FnGetAtt $ec2SecurityGroup 'GroupId') -DependsOn $ec2SecurityGroup
+$newVSEC2SecurityGroupSplat = @{
+    GroupDescription = 'Port 1433 access to RDS from local only'
+    SecurityGroupIngress = $securityGroupIngress
+    LogicalId = 'RDSSecurityGroup'
+}
+$ec2SecurityGroup = New-VSEC2SecurityGroup @newVSEC2SecurityGroupSplat
+
+$newVSRDSDBInstanceSplat = @{
+    AllocatedStorage = '25'
+    MasterUserPassword = $secretValue
+    LogicalId = "SqlServerExpress"
+    EngineVersion = "13.00.4451.0.v1"
+    DBInstanceIdentifier = 'cf-sqlserver-ex-1'
+    PubliclyAccessible = $true
+    VPCSecurityGroups = (Add-FnGetAtt $ec2SecurityGroup 'GroupId')
+    MasterUsername = 'rdsmaster'
+    StorageType = 'gp2'
+    DependsOn = $ec2SecurityGroup
+    AvailabilityZone = 'us-west-2a'
+    MultiAZ = $false
+    Engine = 'sqlserver-ex'
+    DBInstanceClass = 'db.t2.micro'
+}
+$rdsInstance = New-VSRDSDBInstance @newVSRDSDBInstanceSplat
 
 $template.AddResource($customResource,$ec2SecurityGroup,$rdsInstance)
 $template.AddOutput(
     (New-VaporOutput -LogicalId RDSMasterPassword -Value $secretValue)
 )
 
-New-VSStack -TemplateBody $template -StackName "my-sql-express-stack" -ProfileName dev -WhatIf
+$newVSStackSplat = @{
+    TemplateBody = $template
+    StackName = "my-sql-express-stack"
+    ProfileName = 'dev'
+    WhatIf = $true
+}
+New-VSStack @newVSStackSplat
